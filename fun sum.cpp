7d8c73a41b6e55d51eeee17fe01f3bb78d717a63
vaishnu205 <dandadevaishnavi07@gmail.com>
2025-09-20 13:07:18 +0530

    Create fun sum.cpp

---
+++ fun sum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int sum(int a,int b){
+    return a+b;
+}
+int main(){
+    int a;
+    cout<<"enter a :";
+    cin>>a;
+    int b;
+    cout<<"enter b:";
+    cin>>b;
+    cout<<sum(a,b);
+}
